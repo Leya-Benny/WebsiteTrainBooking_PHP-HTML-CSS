a8d9dd010fbfaee55015ad6c94feb92684dbf84c
Leya Benny <leyabenny10@gmail.com>
2024-06-07 19:06:46 -0400

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Railway</title>
+    <link rel="stylesheet" href="welcome.css">
+    </head>
+    <STYLE>a {text-decoration: none;}</STYLE>
+    <body vlink="white">
+            <ul>
+            <h1><i>Passenger's Dashboard</i></h1>
+            <li><b><a href="index.html" target="_self">Home</a></b></li>
+                <li><b><a href="new.html" target="_self">Booking</a></b></li>
+                <li><b> <a href="logout.php">Logout</a></b></li>
+            </ul>
+              <center> <h3 class="m-0">Quick Tips</h3></center>
+                        <div class="card-body">
+                            Click on booking<br> The system will display list of available schedules for you which you can view and make bookings from.<br>
+Before your bookings are saved, you are redirected to make payment.<br>
+After a successful payment, system generates your ticket ID for you which you are required to bring to the station.
+</div>
+</body>
+</html>
