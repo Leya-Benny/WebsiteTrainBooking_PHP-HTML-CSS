c671926cf9576886467913bf65bd52f25e956f83
Leya Benny <leyabenny10@gmail.com>
2025-05-15 14:04:17 -0400

    Update one.php

--- one.php
+++ one.php
@@ -1,67 +1,61 @@
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
     <title>Booking</title>
     <link rel="stylesheet" href="all.css">
-    </head>
-    <body>
-        <div>
-            <div>
-                <form style="position: relative;" action="see.php" method="POST">
-                    <div>
-                    <h2>Enter Details</h2>
-                    <div>
-                        <div>
-                            <label>Full Name</label><br>
-                            <input type="text" required minlenght="10" name="name">
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <label>contact Number</label><br>
-                            <input type="text" minlength="10" required name="phone" ><br>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <label>Email Address</label><br>
-                            <input type="email" name="email" >
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <label> Select Picture</label><br>
-                            <input type="file" name="file" >
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <label>Address</label><br>
-                            <input type="text" name="address" >
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                    <label>Time:</label><center><table><tr><td><input  type="radio" id="t1" name="time" value="6:00AM"><label>6:00AM- Malabar Express</label></td>
-                    <td><input  type="radio" id="t2" name="time" value="12:00PM"><label>12:00AM- Superfast Express</label></td>
-                    <td><input type="radio" id="t3" name="time" value="10:00PM"><label>10:00PM- Lavanya Express</label></td></tr></table></center>
-</div>
-</div><br>
-<div>
-                        <div>
-                    <time>Date:</label><center><table><tr><td><input  type="radio" id="d1" name="date" value="22/06/2022"><label>22/06/2022</label></td>
-                    <td><input  type="radio" id="d2" name="date" value="25/06/2022"><label>25/06/2022</label></td>
-                    <td><input  type="radio" id="d3" name="date" value="22/06/2022"><label>22/06/2022</label></td></tr></table></center>
-</div>
-</div>
-                    <div>
-                        <div>
-                            <br><button type="submit"><b>SUBMIT</b></button>
-                        </div>
-                    </div>
-                    </div>
-                </form>
-            </div>
+    <style>
+        body { font-family: Arial, sans-serif; padding: 20px; }
+        form { max-width: 600px; margin: auto; }
+        label { display: block; margin-top: 15px; font-weight: bold; }
+        input[type="text"], input[type="email"], input[type="file"] {
+            width: 100%; padding: 8px; margin-top: 5px;
+        }
+        .radio-group { margin-top: 10px; }
+        .radio-group label { font-weight: normal; margin-right: 15px; }
+        .submit-btn {
+            margin-top: 20px;
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: white;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <form action="see.php" method="POST" enctype="multipart/form-data">
+        <h2>Enter Booking Details</h2>
+
+        <label>Full Name</label>
+        <input type="text" name="name" required minlength="3">
+
+        <label>Contact Number</label>
+        <input type="text" name="phone" required minlength="10">
+
+        <label>Email Address</label>
+        <input type="email" name="email">
+
+        <label>Select Picture</label>
+        <input type="file" name="file">
+
+        <label>Address</label>
+        <input type="text" name="address">
+
+        <label>Time:</label>
+        <div class="radio-group">
+            <input type="radio" name="time" value="6:00AM" required> 6:00AM - Malabar Express
+            <input type="radio" name="time" value="12:00PM"> 12:00PM - Superfast Express
+            <input type="radio" name="time" value="10:00PM"> 10:00PM - Lavanya Express
         </div>
-    </body>
+
+        <label>Date:</label>
+        <div class="radio-group">
+            <input type="radio" name="date" value="22/06/2025" required> 22/06/2025
+            <input type="radio" name="date" value="25/06/2025"> 25/06/2025
+            <input type="radio" name="date" value="30/06/2025"> 30/06/2025
+        </div>
+
+        <button class="submit-btn" type="submit" name="submit">Book Ticket</button>
+    </form>
+</body>
 </html>
+
